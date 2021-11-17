b744e74f637be088c767b9e0dea949847439df35
amirrezamahmoudzadeh <81969097+amirrezamahmoudzadeh@users.noreply.github.com>
2021-11-17 10:10:55 +0330

    Add files via upload

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-import { Container, Typography, Button, Grid } from "@mui/material";
+import { Container, Typography, Button, Grid, Modal , Box } from "@mui/material";
 import ItemCart from "./ItemCart";
 import { useMediaQuery } from "@mui/material";
-
 
 function Cart({
   cart,
@@ -29,6 +28,9 @@ function Cart({
   }
 
   const FilledCart = () => {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
     return (
       <>
         <Grid
@@ -55,7 +57,7 @@ function Cart({
             marginTop: "2rem",
           }}
         >
-          <Typography variant="h5" color="initial" style={{ direction: "rtl"}}>
+          <Typography variant="h5" color="initial" style={{ direction: "rtl" }}>
             قیمت کل : {cart.subtotal.formatted_with_symbol}
           </Typography>
           <div style={{ textAlign: "right" }}>
@@ -64,21 +66,55 @@ function Cart({
               size="large"
               type="button"
               color="error"
-              style={{ minWidth: "160px", margin: matches ? " 0 1rem 0 0" : "0" }}
+              style={{
+                minWidth: "160px",
+                margin: matches ? " 0 1rem 0 0" : "0",
+              }}
               onClick={emptyCartHandler}
             >
               پاک کردن سبد خرید
             </Button>
             <Button
+              onClick={handleOpen}
               variant="contained"
               size="large"
               type="button"
               color="success"
-              style={{ minWidth: "160px",  margin: matches ? " 0 1rem 0 0" : "1rem 0 0 0"}}
+              style={{
+                minWidth: "160px",
+                margin: matches ? " 0 1rem 0 0" : "1rem 0 0 0",
+              }}
             >
               پرداخت
             </Button>
           </div>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box
+              sx={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                transform: "translate(-50%, -50%)",
+                width: 400,
+                bgcolor: "#37e71f",
+                // border: "2px solid #000",
+                boxShadow: 24,
+                p: 5,
+              }}
+            >
+              <Typography id="modal-modal-title" variant="h6" component="h2" sx={{direction: "rtl"}}>
+                با تشکر  از خرید شما 
+              </Typography>
+              <Typography id="modal-modal-title" variant="subtitle1" sx={{direction: "rtl"}}>
+                سفارش ما در اسرع وقت به دست شما خواهد رسید 
+              </Typography>
+            </Box>
+          </Modal>
         </div>
       </>
     );
